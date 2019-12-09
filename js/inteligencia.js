0dc6bcb5f9c428b0f5abe99b290c5cc065c2e33b
felipessferreira <48361825+felipessferreira@users.noreply.github.com>
2019-12-09 20:27:11 -0300

    Add files via upload

--- js/inteligencia.js
+++ js/inteligencia.js
@@ -1,33 +1,26 @@
 
 function VerificarConfronto(form){
-	
 	var timeCasa = document.getElementById("timeCasa").value;
-    	timeCasa = parseInt(timeCasa) + 15;
+    timeCasa = parseInt(timeCasa) + 15;
 	var select = document.querySelector('select');
 	var option = select.children[select.selectedIndex];
 	var time1 = option.textContent;
 	
-    	var timeVisitante = document.getElementById("timeVisitante").value;
+    var timeVisitante = document.getElementById("timeVisitante").value;
 	var select = document.querySelector('#timeVisitante');
 	var option = select.children[select.selectedIndex];
-    	var time2 = option.textContent;
+    var time2 = option.textContent;
 	
-	if(time1 != time2){
-
+	if(time1 != time2 && time1 != " Casa" && time2 != " Visitante"){
         if(timeCasa > timeVisitante){
-            //alert(time1 + "vencerá o jogo!");
-
             document.getElementById('divResultado').innerHTML = time1 + "vencerá o jogo!";
-            
 
-        }else if(timeVisitante > timeCasa){
-            //alert(time2 + "vencerá o jogo!");
-            document.getElementById('divResultado').innerHTML = time2 + "vencerá o jogo!";
-        } else{
-            //alert("O jogo irá empatar!")
-            document.getElementById('divResultado').innerHTML = "O jogo irá empatar!"
+            }else if(timeVisitante > timeCasa){
+                document.getElementById('divResultado').innerHTML = time2 + "vencerá o jogo!";
+            } else{
+                document.getElementById('divResultado').innerHTML = "O jogo irá empatar!"
+            }
+        }else{
+            alert("Os juízes do Futcomp não conseguem analisar este confronto, selecione equipes diferentes!");
         }
-    }else{
-        alert("Os juízes do Futcomp não conseguem analisar este confronto, selecione equipes diferentes!");
-    }
 }
